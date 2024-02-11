cd4648e2484740fb9262ccc6cb2358b617e9f55a
Amin Mousavi <seyedamin963@gmail.com>
2024-02-11 14:57:53 +0330

    Build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,29 @@
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`,
+        }}
+      >
+        <GridItem area="nav" bg="coral">
+          nav
+        </GridItem>
+        <Show above="lg">
+          {" "}
+          <GridItem area="aside" bg="gold">
+            aside
+          </GridItem>
+        </Show>
+        <GridItem area="main" bg="skyblue">
+          main
+        </GridItem>
+      </Grid>
+    </>
+  );
 }
 
 export default App;
